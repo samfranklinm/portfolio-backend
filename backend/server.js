f1423edfb141598b4dd2dd8ef27a83b58b41bde6
Sam Mohan <samfranklinm@gmail.com>
2024-11-21 11:51:36 +0700

    add trouble shoot log

--- backend/server.js
+++ backend/server.js
@@ -167,6 +167,7 @@ app.use((err,res) => {
 
 // Start the Server
 const PORT = process.env.PORT || 5003;
+console.log(`Environment PORT: ${process.env.PORT}`);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
